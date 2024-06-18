598d145f59fddb389274e74c665a3f18bf984dca
Erick Ramos <erickramossilva@gmail.com>
2024-06-18 09:31:16 -0300

    add project

---
+++ src/main/java/com/alura/forumbhub/ForumbhubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.forumbhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumbhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumbhubApplication.class, args);
+	}
+
+}
